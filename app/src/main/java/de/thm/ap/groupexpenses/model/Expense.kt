74069171b26f799533f72fd4ddf394b75f2af276
Tim Robin Schweizer <tim.schweizer@mni.thm.de>
2020-12-27 23:37:54 +0100

    Add Expense data class

---
+++ app/src/main/java/de/thm/ap/groupexpenses/model/Expense.kt
@@ -0,0 +1,44 @@
+package de.thm.ap.groupexpenses.model
+
+import java.util.*
+
+data class Expense(
+        /**
+         * The group in which the expense was made.
+         */
+        val group: Group,
+        /**
+         * The user who has paid.
+         */
+        val payedUser: User,
+        /**
+         * The date of the expense.
+         */
+        val date: Date,
+        /**
+         * The name of the expense.
+         */
+        val name: String,
+        /**
+         * The cost of the expense in cents.
+         * (prevent arithmetic errors produced by floats or doubles)
+         */
+        val cost: Int) {
+
+    private var _receipts: List<String>? = null
+
+    /**
+     * The uris to receipts, bound to the expense.
+     */
+    var receipts: List<String>
+        get() {
+            if (this._receipts == null) {
+                TODO("initialize from database")
+            }
+
+            return this._receipts!!
+        }
+        set(value) {
+            TODO()
+        }
+}
