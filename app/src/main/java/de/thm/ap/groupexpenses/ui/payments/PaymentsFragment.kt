12692bb5c6df64ac56c69573c813191766de400e
Renke Hohl <renke.hohl@mni.thm.de>
2021-01-06 17:29:21 +0100

    implemented login

---
+++ app/src/main/java/de/thm/ap/groupexpenses/ui/payments/PaymentsFragment.kt
@@ -0,0 +1,4 @@
+package de.thm.ap.groupexpenses.ui.payments
+
+class PaymentsFragment {
+}
---
+++ app/src/main/java/de/thm/ap/groupexpenses/ui/settings/SettingsFragment.kt
@@ -0,0 +1,38 @@
+package de.thm.ap.groupexpenses.ui.settings
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import de.thm.ap.groupexpenses.databinding.FragmentSettingsBinding
+
+class SettingsFragment : Fragment() {
+    private val settingsViewModel: SettingsViewModel by viewModels()
+    private var _binding: FragmentSettingsBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textSettings
+        settingsViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
