146257fb4377a933d62709d538490af9d07baf77
jfhn47 <jan.hindges@mni.thm.de>
2021-01-27 18:21:44 +0100

    Add Group members page in Group activity

---
+++ app/src/main/java/de/thm/ap/groupexpenses/adapter/GroupMembersAdapter.kt
@@ -0,0 +1,35 @@
+package de.thm.ap.groupexpenses.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.ktx.toObject
+import de.thm.ap.groupexpenses.databinding.ItemGroupMemberBinding
+import de.thm.ap.groupexpenses.model.GroupMember
+
+open class GroupMembersAdapter(query: Query)
+    : FirestoreAdapter<GroupMembersAdapter.ViewHolder>(query)
+{
+    companion object {
+        const val TAG = "GroupMembersAdapter"
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(ItemGroupMemberBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getSnapshot(position))
+    }
+
+    class ViewHolder(val binding: ItemGroupMemberBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(snapshot: DocumentSnapshot) {
+            val groupMember: GroupMember = snapshot.toObject() ?: return
+
+            binding.itemGroupMemberName.text = groupMember.userName
+            binding.itemGroupMemberRole.text = groupMember.role
+        }
+    }
+}
---
+++ app/src/main/java/de/thm/ap/groupexpenses/ui/group/GroupMembersFragment.kt
@@ -0,0 +1,47 @@
+package de.thm.ap.groupexpenses.ui.group
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import de.thm.ap.groupexpenses.GroupViewModel
+import de.thm.ap.groupexpenses.adapter.GroupMembersAdapter
+import de.thm.ap.groupexpenses.databinding.FragmentGroupMembersBinding
+import de.thm.ap.groupexpenses.ui.RecyclerFragment
+
+class GroupMembersFragment : RecyclerFragment() {
+    private lateinit var binding: FragmentGroupMembersBinding
+    private val groupViewModel: GroupViewModel by activityViewModels()
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?)
+    : View {
+        binding = FragmentGroupMembersBinding.inflate(inflater, container, false)
+        initRecyclerView()
+        return binding.root
+    }
+
+    override fun initRecyclerView() {
+        val query: Query = Firebase.firestore
+                .collection("groups/${groupViewModel.groupId}/members")
+                .orderBy("role", Query.Direction.ASCENDING)
+
+        adapter = GroupMembersAdapter(query)
+
+        binding.recyclerGroupMembers.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerGroupMembers.adapter       = adapter
+
+        adapter?.startListening()
+    }
+
+    companion object {
+        const val TAG = "GroupMembersFragment"
+    }
+}
