a74b7b67cc9d3fdfb800da798843cb539305c3e5
Tim Robin Schweizer <tim.schweizer@mni.thm.de>
2021-02-23 18:27:17 +0100

    Add delete button for payments

--- app/src/main/java/de/thm/ap/groupexpenses/adapter/GroupPaymentsAdapter.kt
+++ app/src/main/java/de/thm/ap/groupexpenses/adapter/GroupPaymentsAdapter.kt
@@ -1,18 +1,27 @@
 package de.thm.ap.groupexpenses.adapter
 
+import android.app.AlertDialog
+import android.content.Context
 import android.icu.text.SimpleDateFormat
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
 import de.thm.ap.groupexpenses.R
 import de.thm.ap.groupexpenses.databinding.ItemGroupPaymentBinding
 import de.thm.ap.groupexpenses.model.GroupPayment
+import de.thm.ap.groupexpenses.worker.FirebaseWorker
 import java.util.*
 
-open class GroupPaymentsAdapter(query: Query)
+open class GroupPaymentsAdapter(query: Query,
+                                private val isAdmin: Boolean,
+                                private val context: Context,
+                                private val groupId: String)
     : FirestoreAdapter<GroupPaymentsAdapter.ViewHolder>(query) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -23,7 +32,7 @@ open class GroupPaymentsAdapter(query: Query)
         holder.bind(getSnapshot(position))
     }
 
-    class ViewHolder(val binding: ItemGroupPaymentBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolder(val binding: ItemGroupPaymentBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(snapshot: DocumentSnapshot) {
             val groupPayment: GroupPayment = snapshot.toObject() ?: return
             val resources = binding.root.resources
@@ -35,6 +44,25 @@ open class GroupPaymentsAdapter(query: Query)
                     resources.getString(R.string.fmt_double_EUR),
                     groupPayment.payment
             )
+
+            binding.itemGroupPaymentDelete.visibility =
+                if (isAdmin || groupPayment.userId == Firebase.auth.currentUser?.uid) {
+                    binding.itemGroupPaymentDelete.setOnClickListener {
+                        AlertDialog.Builder(context).apply {
+                            setTitle(R.string.delete_payment)
+                            val message = context.getString(R.string.confirm_delete_payment)
+                            setMessage(message)
+                            setNeutralButton(R.string.cancel, null)
+                            setNegativeButton(R.string.delete_payment) { _, _ ->
+                                FirebaseWorker.deletePayment(groupId, groupPayment)
+                            }
+                            show()
+                        }
+                    }
+                    View.VISIBLE
+                } else {
+                    View.INVISIBLE
+                }
         }
     }
 
--- app/src/main/java/de/thm/ap/groupexpenses/ui/group/GroupPaymentsFragment.kt
+++ app/src/main/java/de/thm/ap/groupexpenses/ui/group/GroupPaymentsFragment.kt
@@ -7,13 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
 import de.thm.ap.groupexpenses.GroupActivity.Companion.KEY_GROUP_ID
 import de.thm.ap.groupexpenses.GroupViewModel
 import de.thm.ap.groupexpenses.PaymentFormActivity
 import de.thm.ap.groupexpenses.adapter.GroupPaymentsAdapter
 import de.thm.ap.groupexpenses.databinding.FragmentGroupPaymentsBinding
+import de.thm.ap.groupexpenses.model.GroupMember
 import de.thm.ap.groupexpenses.ui.RecyclerFragment
+import de.thm.ap.groupexpenses.worker.FirebaseWorker
 import de.thm.ap.groupexpenses.worker.FirebaseWorker.groupPaymentsQuery
 
 class GroupPaymentsFragment : RecyclerFragment() {
@@ -40,21 +45,31 @@ class GroupPaymentsFragment : RecyclerFragment() {
     override fun initRecyclerView() {
         val query: Query = groupPaymentsQuery(groupViewModel.groupId)
 
-        adapter = object : GroupPaymentsAdapter(query) {
-            override fun onDataChanged() {
-                if (itemCount == 0) {
-                    binding.recyclerGroupPayments.visibility  = View.GONE
-                    binding.groupPaymentsEmptyView.visibility = View.VISIBLE
-                } else {
-                    binding.recyclerGroupPayments.visibility  = View.VISIBLE
-                    binding.groupPaymentsEmptyView.visibility = View.GONE
+        FirebaseWorker.getGroupMember(groupViewModel.groupId, Firebase.auth.currentUser!!.uid)
+            .addOnSuccessListener {
+                val isAdmin = it!!.toObject<GroupMember>()!!.role == "admin"
+
+                adapter = object : GroupPaymentsAdapter(
+                    query,
+                    isAdmin,
+                    requireContext(),
+                    groupViewModel.groupId
+                ) {
+                    override fun onDataChanged() {
+                        if (itemCount == 0) {
+                            binding.recyclerGroupPayments.visibility = View.GONE
+                            binding.groupPaymentsEmptyView.visibility = View.VISIBLE
+                        } else {
+                            binding.recyclerGroupPayments.visibility = View.VISIBLE
+                            binding.groupPaymentsEmptyView.visibility = View.GONE
+                        }
+                    }
                 }
+
+                binding.recyclerGroupPayments.layoutManager = LinearLayoutManager(requireContext())
+                binding.recyclerGroupPayments.adapter = adapter
+
+                adapter?.startListening()
             }
-        }
-
-        binding.recyclerGroupPayments.layoutManager = LinearLayoutManager(requireContext())
-        binding.recyclerGroupPayments.adapter = adapter
-
-        adapter?.startListening()
     }
 }
