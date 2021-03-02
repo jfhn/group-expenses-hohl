016bb52159461e9e44534b354a1dc84ee2befc23
jfhn47 <jan.hindges@mni.thm.de>
2021-03-02 14:01:05 +0100

    Add remaining documentation to model package

--- app/src/main/java/de/thm/ap/groupexpenses/model/GroupPayment.kt
+++ app/src/main/java/de/thm/ap/groupexpenses/model/GroupPayment.kt
@@ -4,10 +4,36 @@ import com.google.firebase.firestore.DocumentId
 import com.google.firebase.firestore.ServerTimestamp
 import java.util.*
 
+/**
+ * This class represents the document of a group payment in the firestore database.
+ */
 class GroupPayment {
+    /**
+     * The document id for the group payment in the firebase firestore database.
+     *
+     * firestore path: groups/$groupId/payments/$id
+     */
     @DocumentId var id: String? = null
+
+    /**
+     * The document id for the user that made the payment in the firebase firestore database.
+     *
+     * firestore path: users/$userId
+     */
     var userId: String? = null
+
+    /**
+     * The user name of the user that made the payment.
+     */
     var userName: String? = null
+
+    /**
+     * The date the payment was made.
+     */
     @ServerTimestamp var date: Date? = null
+
+    /**
+     * The amount of money of the payment.
+     */
     var payment: Double = 0.0
 }
--- app/src/main/java/de/thm/ap/groupexpenses/model/UserPayment.kt
+++ app/src/main/java/de/thm/ap/groupexpenses/model/UserPayment.kt
@@ -3,9 +3,30 @@ package de.thm.ap.groupexpenses.model
 import com.google.firebase.firestore.ServerTimestamp
 import java.util.*
 
+/**
+ * This class represents the document of a user payment in the firestore database.
+ */
 class UserPayment {
+    /**
+     * The document id for the group where the payment was made in the firebase firestore
+     * database.
+     *
+     * firestore path: groups/$groupId
+     */
     var groupId: String? = null
+
+    /**
+     * The group name.
+     */
     var groupName: String? = null
+
+    /**
+     * The date the payment was made.
+     */
     @ServerTimestamp var date: Date? = null
+
+    /**
+     * The amount of money of the payment.
+     */
     var payment: Double = 0.0
 }
